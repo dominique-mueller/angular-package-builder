156059a7d66534175afe4cfb87fb7a6d0ca2a608
Dominique <dominique.m.mueller@gmail.com>
2017-09-14 21:19:01 +0200

    Add SASS compiler, add CSS minifier

--- src/tasks/inline-resources.ts
+++ src/tasks/inline-resources.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 
+import * as CleanCSS from 'clean-css';
 import * as htmlMinifier from 'html-minifier';
+import * as sass from 'node-sass';
 import * as typescript from 'typescript';
 
 import { AngularPackageBuilderInternalConfig } from './../interfaces/angular-package-builder-internal-config.interface';
@@ -16,7 +18,7 @@ import { AngularResourceAnalyzer } from './../angular-resource-analyzer';
  * Inline resources (HTML templates for now); this also copies files without resources as well as typing definitions files.
  */
 export function inlineResources( config: AngularPackageBuilderInternalConfig, memoryFileSystem: MemoryFileSystem | null ): Promise<void> {
-	return new Promise<void>( async( resolve: () => void, reject: ( error: Error ) => void ) => {
+	return new Promise<void>( async ( resolve: () => void, reject: ( error: Error ) => void ) => {
 
 		// Import
 		const writeFile = ( await dynamicImport( './../utilities/write-file', memoryFileSystem ) ).writeFile;
@@ -25,14 +27,14 @@ export function inlineResources( config: AngularPackageBuilderInternalConfig, me
 		// TODO: Exit with error if there are no files?
 		const sourceFilesPatterns: Array<string> = [
 			path.join( '**', '*.ts' ), // Includes typing files
-			`!${ path.join( '**', '*.spec.ts' ) }`,
+			`!${path.join( '**', '*.spec.ts' )}`,
 			...config.ignored
 		];
 		const filePaths: Array<string> = await getFiles( sourceFilesPatterns, config.entry.folder );
 
 		// Inline resources into source files
 		await Promise.all(
-			filePaths.map( async( filePath: string ): Promise<void> => {
+			filePaths.map( async ( filePath: string ): Promise<void> => {
 
 				// Read file
 				const absoluteSourceFilePath: string = path.join( config.entry.folder, filePath );
@@ -77,7 +79,7 @@ function inlineExternalResources( externalResources: Array<any>, fileContent: st
 
 		// Replace value(s)
 		newFileContent = externalResource.urls.reduce( ( newFileContent: string, url: any ): string => {
-			newFileContent = replaceAt( newFileContent, `\`${ url.content }\``, url.node, currentPositionCorrection );
+			newFileContent = replaceAt( newFileContent, `\`${url.content}\``, url.node, currentPositionCorrection );
 			currentPositionCorrection += url.content.length - url.url.length;
 			return newFileContent;
 		}, newFileContent );
@@ -94,9 +96,9 @@ function inlineExternalResources( externalResources: Array<any>, fileContent: st
 async function loadExternalResources( externalResources: Array<any>, filePath: string ): Promise<Array<any>> {
 
 	return Promise.all(
-		externalResources.map( async( externalResource: any ): Promise<any> => {
+		externalResources.map( async ( externalResource: any ): Promise<any> => {
 			externalResource.urls = await Promise.all(
-				externalResource.urls.map( async( url: any ): Promise<any> => {
+				externalResource.urls.map( async ( url: any ): Promise<any> => {
 					url.content = await loadExternalResource( url.url, filePath );
 					return url;
 				} )
@@ -114,29 +116,27 @@ async function loadExternalResource( resourceUrl: string, filePath: string ): Pr
 
 	// Read the resource file
 	const absoluteTemplatePath: string = path.join( path.dirname( filePath ), resourceUrl );
-	const resource: string = await readFile( absoluteTemplatePath );
+	let resource: string = await readFile( absoluteTemplatePath );
 
 	// Prepare files, based on file type
 	const fileType: string = path.extname( resourceUrl ).substring( 1 );
-	let preparedResource: string;
 	switch ( fileType ) {
 
 		// External HTML templates
 		case 'html':
-			preparedResource = htmlMinifier.minify( resource, htmlMinifierConfig );
+			resource = minifyHtml( resource );
 			break;
 
 		// External CSS files
 		case 'css':
-			preparedResource = resource.replace( /([\n\r]\s*)+/gm, '' ) // TODO: ...
+			resource = minifyCss( resource );
 			break;
 
 		// External SASS files
 		case 'scss':
-			preparedResource = resource.replace( /([\n\r]\s*)+/gm, '' ) // TODO: ...
+			resource = await compileSass( resource );
+			resource = minifyCss( resource );
 			break;
-
-		// TODO: What about .sass or .less??
 
 		// Unknown resource types
 		default:
@@ -144,8 +144,38 @@ async function loadExternalResource( resourceUrl: string, filePath: string ): Pr
 
 	}
 
-	return preparedResource;
+	return resource;
 
+}
+
+function minifyHtml( htmlContent: string ): string {
+	return htmlMinifier.minify( htmlContent, htmlMinifierConfig );
+}
+
+function minifyCss( cssContent: string ): string {
+
+	const result: any = new CleanCSS( {
+		level: 0 // No optimization
+	} ).minify( cssContent );
+
+	return result.styles;
+
+}
+
+function compileSass( sassContent: string ): Promise<string> {
+	return new Promise<string>( ( resolve: ( cssContent: string ) => void, reject: ( error: Error ) => void ) => {
+
+		// Compile SASS into CSS
+		sass.render( {
+			data: sassContent,
+			outputStyle: 'expanded' // We will minify later on
+		}, ( error: Error, sassRenderResult: any ) => {
+
+			resolve( sassRenderResult.css.toString() );
+
+		} );
+
+	} );
 }
 
 /**
