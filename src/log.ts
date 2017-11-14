c27ab4f59ad73d0952322108017fd5528a55fe09
Dominique <dominique.m.mueller@gmail.com>
2017-11-15 00:14:47 +0100

    Add logger

---
+++ src/log.ts
@@ -0,0 +1,41 @@
+import chalk from 'chalk';
+
+// Different arrow symbol based on OS
+const arrowSymbol = process.platform === 'win32' ? '→' : '➜';
+
+/**
+ * Logger
+ *
+ * @param [type='default'] - Log type
+ * @param [message='']     - Log message
+ */
+export function log( type: 'title' | 'success' | 'error' | 'step' | 'default' = 'default', message: string = '' ): void {
+
+	switch( type ) {
+
+		case 'title':
+			console.log( chalk.white.bold.underline( message ) );
+			break;
+
+		case 'success':
+			console.log( chalk.green.bold( message ) );
+			break;
+
+		case 'error':
+			console.log( chalk.red.bold( message ) );
+			break;
+
+		case 'step':
+			console.log( chalk.white.bold( `  ${ arrowSymbol } ${ message }` ) );
+			break;
+
+		// case 'substep':
+		// 	console.log( chalk.gray( `    ${ arrowSymbol } ${  message }` ) );
+		// 	break;
+
+		default:
+			console.log( message );
+
+	}
+
+}
