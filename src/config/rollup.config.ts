965562a41258d43008ea36fd5be13bc4a68b147c
Dominique <dominique.m.mueller@gmail.com>
2018-03-21 21:43:14 +0100

    Silent rollup optimization warning messages

--- src/config/rollup.config.ts
+++ src/config/rollup.config.ts
@@ -19,6 +19,18 @@ export async function getRollupInputConfig( sourcePath: string, target: 'ES2015'
 		input: path.join( sourcePath, `${ parsePackageJsonName( config.packageName ).fullName }.js` ), // Previously 'entry' which is now deprecated
 		onwarn: ( warning: RollupWarning ): void => {
 
+			// Supress THIS_IS_UNDEFINED warnings, as they're not having an effect on the bundle
+			// - Documentation: https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+			// - Recommendation: https://github.com/rollup/rollup/issues/794#issuecomment-260694288
+			if ( warning.indexOf( 'THIS_IS_UNDEFINED' ) !== -1 ) {
+				return;
+			}
+
+			// Supress UNUSED_EXTERNAL_IMPORT warnings, as they're optimzation warnings
+			if ( warning.indexOf( 'UNUSED_EXTERNAL_IMPORT' ) !== -1 ) {
+				return;
+			}
+
 			// Print prettier warning log
 			const betterWarningMessage: string = warning.message
 				.replace( /\\/g, '/' )
