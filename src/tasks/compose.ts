5fc4cd591a4b9d1913c32b862f49368da1687184
Dominique Müller <dominique.m.mueller@gmail.com>
2018-03-25 14:43:36 +0200

    Only copy js and sourcemap build files

--- src/tasks/compose.ts
+++ src/tasks/compose.ts
@@ -11,14 +11,14 @@ import { copyFiles } from '../utilities/copy-files';
 export async function compose( config: AngularPackageBuilderInternalConfig ): Promise<void> {
 	await Promise.all( [
 
-		// Single files
-		copyFiles( path.join( config.temporary.esm2015, '**' ), path.join( config.output.folder, 'esm2015' ) ),
-		copyFiles( path.join( config.temporary.esm5, '**' ), path.join( config.output.folder, 'esm5' ) ),
+		// Build files
+		copyFiles( path.join( config.temporary.esm2015, '**', '*.js?(.map)' ), path.join( config.output.folder, 'esm2015' ) ),
+		copyFiles( path.join( config.temporary.esm5, '**', '*.js?(.map)' ), path.join( config.output.folder, 'esm5' ) ),
 
 		// Bundles
-		copyFiles( path.join( config.temporary.fesm2015, '**' ), path.join( config.output.folder, 'fesm2015' ) ),
-		copyFiles( path.join( config.temporary.fesm5, '**' ), path.join( config.output.folder, 'fesm5' ) ),
-		copyFiles( path.join( config.temporary.umd, '**' ), path.join( config.output.folder, 'bundles' ) ),
+		copyFiles( path.join( config.temporary.fesm2015, '**', '*.js?(.map)' ), path.join( config.output.folder, 'fesm2015' ) ),
+		copyFiles( path.join( config.temporary.fesm5, '**', '*.js?(.map)' ), path.join( config.output.folder, 'fesm5' ) ),
+		copyFiles( path.join( config.temporary.umd, '**', '*.js?(.map)' ), path.join( config.output.folder, 'bundles' ) ),
 
 		// TypeScript definition files
 		copyFiles( path.join( config.temporary.esm2015, '**', '*.d.ts' ), config.output.folder ),
