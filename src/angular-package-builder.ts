e5125e7c066ee8d61dd2e38b1c27ec1fcf463344
Dominique <dominique.m.mueller@gmail.com>
2018-05-29 23:10:14 +0200

    Fix TypeScript sourcemap paths

--- src/angular-package-builder.ts
+++ src/angular-package-builder.ts
@@ -1,11 +1,12 @@
 import { posix as path } from 'path';
 
+import * as del from 'del';
+
 import { AngularPackage } from './angular-package';
 import { AngularPackageBundler } from './bundler/angular-package.bundler';
 import { AngularPackageCompiler } from './compiler/angular-package.compiler';
 import { AngularPackageTransformer } from './transformer/angular-package.transformer';
 import { AngularPackageComposer } from './composer/angular-package.composer';
-import { deleteFolder } from './utilities/delete-folder';
 import { AngularPackageLogger } from './logger/angular-package-logger';
 
 /**
@@ -21,7 +22,7 @@ export class AngularPackageBuilder {
      */
     public static async package( angularPackage: AngularPackage ): Promise<void> {
 
-        await this.cleanupTemporaryOutputFolder( angularPackage );
+        await this.cleanupOutputFolder( angularPackage ); // Includes the temp output folder
 
         await this.transform( angularPackage );
         await this.compile( angularPackage );
@@ -109,13 +110,23 @@ export class AngularPackageBuilder {
     }
 
     /**
-     * Cleanup the temporary output folder
+     * Cleaniup the output folder (meaning the folder itself and all its content), including the temporary output folder
+     *
+     * @param   angularPackage Angular Package
+     * @returns                Promise, resolves when done
+     */
+    private static async cleanupOutputFolder( angularPackage: AngularPackage ): Promise<void> {
+        await del( [ path.join( angularPackage.root, angularPackage.outDir, '**' ) ] ); // The '**' is important!
+    }
+
+    /**
+     * Cleanup the temporary output folder (meaning the folder itself and all its content)
      *
      * @param   angularPackage Angular Package
      * @returns                Promise, resolves when done
      */
     private static async cleanupTemporaryOutputFolder( angularPackage: AngularPackage ): Promise<void> {
-        await deleteFolder( path.join( angularPackage.root, angularPackage.outDir, 'temp' ) );
+        await del( [ path.join( angularPackage.root, angularPackage.outDir, 'temp', '**' ) ] ); // The '**' is important!
 	}
 
 }
