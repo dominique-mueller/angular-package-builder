8d7dddcac14d5ba32ae4349d566590794b2356ff
Dominique Müller <dominique.m.mueller@gmail.com>
2018-06-20 23:58:30 +0200

    feat(compatibility): Add compatibility warnings (#131)
    - Add compatibilty checker with warning logging
    - Extend Travis CI test matrix

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import { AngularPackageOrchestrator } from './src/angular-package-orchestrator';
 import { AngularPackageBuilder } from './src/angular-package-builder';
 import { flattenArray } from './src/utilities/flatten-array';
 import { AngularPackageLogger } from './src/logger/angular-package-logger';
+import { AngularPackageCompatibilityChecker } from './src/angular-package-compatibility-checker';
 
 /**
  * Run Angular Package Builder
@@ -13,6 +14,13 @@ import { AngularPackageLogger } from './src/logger/angular-package-logger';
 export async function runAngularPackageBuilder( angularPackageJsonPaths: Array<string> ): Promise<void> {
 
 	AngularPackageLogger.logTitle( 'Angular Package Builder' );
+
+	// Check versions
+	try {
+		await AngularPackageCompatibilityChecker.checkCompatibility();
+	} catch ( error ) {
+		AngularPackageLogger.logPreparationMessage( error.message, 'warning' );
+	}
 
 	// Create Angular package definitions for each angular package json file
 	let angularPackages: Array<Array<AngularPackage>>;
@@ -23,6 +31,7 @@ export async function runAngularPackageBuilder( angularPackageJsonPaths: Array<s
 		throw new Error( error.message );
 	}
 
+	// Derive build orchestration
 	let buildOrchestration: Array<Array<Array<Array<AngularPackage>>>>;
 	try {
 		buildOrchestration = AngularPackageOrchestrator.orchestrateAngularPackagesBuild( angularPackages );
@@ -31,16 +40,18 @@ export async function runAngularPackageBuilder( angularPackageJsonPaths: Array<s
 		throw new Error( error.message );
 	}
 	const flattenedBuildOrchestration: Array<AngularPackage> = flattenArray( buildOrchestration );
-	AngularPackageLogger.configureNumberOfBuildSteps( flattenedBuildOrchestration.length );
 
-	// RUN!
+	// Execute builds
+	AngularPackageLogger.configureNumberOfBuildSteps( flattenedBuildOrchestration.length );
 	const builtAngularPackages: { [ packageName: string ]: Array<string> } = {};
 	for ( const angularPackage of flattenedBuildOrchestration ) {
 
 		AngularPackageLogger.logBuildStart( angularPackage.packageName );
 
+		// Configure already built packages
 		angularPackage.addCustomModulePaths( builtAngularPackages );
 
+		// Create package
 		try {
 			await AngularPackageBuilder.package( angularPackage );
 		} catch ( error ) {
@@ -48,6 +59,7 @@ export async function runAngularPackageBuilder( angularPackageJsonPaths: Array<s
 			throw new Error( error.message ); // Bubble up
 		}
 
+		// Add package to already built packages
 		builtAngularPackages[ angularPackage.packageName ] = [
 			path.join( angularPackage.root, angularPackage.outDir )
 		];
